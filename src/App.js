03e4615f33c550cea5cd7c283b89beb5fd4b58e7
EVANS AMOAMA <evansamoama@gmail.com>
2023-11-27 22:32:26 +1200

    breadcrumb added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 
 import { Entry } from "./pages/entry/Entry.page";
 import { DefaultLayout } from "./layout/DefaultLayout";
+import { Dashboard } from "./pages/Dashboard/Dashboard.page";
 
 function App() {
   return (
     <div className="App">
-  {/*  <Entry />*/}
-  <DefaultLayout >//Dashboard</DefaultLayout>
+  { /* <Entry />*/}
+   <DefaultLayout ><Dashboard /></DefaultLayout> 
     </div>
   );
-}
+};
 
 export default App;
